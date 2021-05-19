860efcd51ad3377084995f990c89186c570d9a52
Nastya <the_sun.89@mail.ru>
2021-05-19 10:27:46 +0300

    first commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -11,16 +11,18 @@ const login = document.getElementById("login");
 const logout = document.getElementById("logout");
 const notesContainer = document.getElementById("notes-container");
 const all = document.getElementById("all");
+
 notes.hidden = true;
 navbarNav.hidden = true;
 alertPrimary.hidden = true;
 login.hidden = false;
 let currentUser = inputEmail.value;
 inputEmail.value = localStorage.getItem("currentUser");
-console.log(localStorage.getItem("currentUser"));
+
 logout.addEventListener("click", () => (location.hash = "#login"));
 loginButton.addEventListener("click", () => (location.hash = "#notes"));
 all.addEventListener("click", () => (location.hash = "#all"));
+
 const changeLocation1 = () => {
   navbarNav.hidden = true;
   notes.hidden = true;
@@ -30,6 +32,7 @@ const changeLocation1 = () => {
   localStorage.removeItem("currentUser");
   inputEmail.value = "";
 };
+
 const changeLocation = () => {
   if (inputEmail.value) {
     notes.hidden = false;
@@ -39,40 +42,41 @@ const changeLocation = () => {
     array = JSON.parse(
       localStorage.getItem(localStorage.getItem("currentUser"))
     );
-    if (array !== null && inputEmail.value == localStorage.getItem("currentUser")) {
-        notesContainer.insertAdjacentHTML("afterbegin", array);
+    if (
+      array !== null &&
+      inputEmail.value == localStorage.getItem("currentUser")
+    ) {
+      notesContainer.insertAdjacentHTML("afterbegin", array);
     }
-  
   }
 };
 
 const changeLocation2 = () => {
   alertPrimary.hidden = false;
-  
-   if (noteText.value) {
+
+  if (noteText.value) {
     const taskText = noteText.value;
     const taskHTML = ` <div class="alert alert-primary" role="alert">
-    <span>${taskText}</span> 
-     </div>`;
+    <span>${taskText}</span> </div>`;
 
     notesContainer.insertAdjacentHTML("afterbegin", taskHTML);
-
     localStorage.setItem(
       localStorage.getItem("currentUser"),
       JSON.stringify(notesContainer.innerHTML)
     );
     noteText.value = "";
   }
- 
 };
 const changeLocation3 = () => {
- notes.hidden = false;
+  notes.hidden = false;
   navbarNav.hidden = false;
   alertPrimary.hidden = true;
   container.hidden = true;
- notesContainer.hidden = true;
-notes.insertAdjacentHTML("afterbegin", '<strong>Этот раздел находится в  разработке.</strong>');
-
+  notesContainer.hidden = true;
+  notes.insertAdjacentHTML(
+    "afterbegin",
+    "<strong>Этот раздел находится в  разработке.</strong>"
+  );
 };
 
 logout.addEventListener("click", changeLocation1);
